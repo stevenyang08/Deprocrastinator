71a6bc0fac2c9839cb217b508e17def17823ee06
Steven yang <steven_yang9190@hotmail.com>
2016-01-18 16:25:52 -0800

    Step 7

--- RootViewController.m
+++ RootViewController.m
@@ -8,7 +8,12 @@
 
 #import "RootViewController.h"
 
-@interface RootViewController ()
+@interface RootViewController () <UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UITextField *addTextField;
+@property NSMutableArray *toDoItems;
+@property int counter;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeObject;
 
 @end
 
@@ -16,22 +21,102 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.toDoItems = [NSMutableArray new];
+    self.editing = false;
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell Identifier"];
+    cell.textLabel.text = [self.toDoItems objectAtIndex:indexPath.row];
+    //cell.textLabel.textColor
+    return cell;
 }
 
-/*
-#pragma mark - Navigation
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.toDoItems.count;
+}
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self.toDoItems removeObjectAtIndex:indexPath.row];
+    [self.tableView reloadData];
+}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *currenCell = [tableView cellForRowAtIndexPath:indexPath];
+    if (currenCell.textLabel.textColor == [UIColor greenColor]) {
+        currenCell.textLabel.textColor = [UIColor blackColor];
+    }
+    else
+    {
+        currenCell.textLabel.textColor = [UIColor greenColor];
+    }
+}
+
+-(void)setEditing:(BOOL)editing animated:(BOOL)animated
+{
+}
+
+
+#pragma mark - Action
+
+- (IBAction)onAddButtonPressed:(UIButton *)sender {
+    [self.toDoItems addObject:self.addTextField.text];
+    self.addTextField.text = [NSString stringWithFormat:@""];
+    [self.view endEditing:YES];
+    self.counter++;
+    [self.tableView reloadData];
+}
+
+- (IBAction)editButtonTapped:(UIBarButtonItem *)sender {
+    if (self.editing) {
+        self.editing = FALSE;
+        [self.tableView setEditing:FALSE animated:YES];
+        sender.style = UIBarButtonItemStylePlain;
+        sender.title = @"Edit";
+    }
+    else
+    {
+        self.editing = TRUE;
+        [self.tableView setEditing:TRUE animated:YES];
+        sender.style = UIBarButtonItemStyleDone;
+        sender.title = @"Done";
+    }
+    
+    
+}
+/*
+- (IBAction)changePriority:(UISwipeGestureRecognizer *)sender {
+    CGPoint location = [sender locationInView:self.tableView];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
+    location.textLabel.backgroundColor = [UIColor redColor];
+ 
+    if (currentCell.textLabel.backgroundColor == [UIColor redColor])
+    {
+        currentCell.textLabel.backgroundColor = [UIColor yellowColor];
+    } else if (currentCell.textLabel.backgroundColor == [UIColor yellowColor])
+    {
+        currentCell.textLabel.backgroundColor = [UIColor greenColor];
+    }
+    else if (currentCell.textLabel.backgroundColor == [UIColor greenColor])
+    {
+        currentCell.textLabel.backgroundColor = [UIColor redColor];
+    }
+ 
 }
 */
+
 
 @end
