f0db3b5ca4f0ed9921758ca58743137052d400b1
Steven yang <steven_yang9190@hotmail.com>
2016-01-18 16:15:06 -0800

    Step 6

--- RootViewController.h
+++ RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  Deprocrastinator
 //
 //  Created by Steven Yang on 1/18/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UIViewController
 
 @end
-
